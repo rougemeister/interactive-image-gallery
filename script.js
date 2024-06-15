0d877670a6a61e9fbd12307a28e1dee7475af670
kruzznitsch <rougemeister1@gmail.com>
2024-06-15 17:00:05 +0100

    Added functions for open and close lightbox

--- script.js
+++ script.js
@@ -72,3 +72,41 @@ const images = [
         description: 'A black porsche car riding home!'
     },
 ]
+
+
+
+const galleryContainer = document.querySelector('#gallery');
+const lightbox = document.querySelector('#lightbox');
+const lightboxImg = document.querySelector('#lightbox-img');
+const captionText = document.querySelector('#caption');
+const description = document.querySelector('.description')
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+const closeButton = document.querySelector('#close');
+
+let currentIndex = 0;
+
+images.forEach((image, index) => {
+    const imgElement = document.createElement('img');
+    console.log(imgElement)
+    imgElement.src = image.thumb;
+   
+    imgElement.alt = image.caption;
+    imgElement.classList.add('thumbnail');
+    imgElement.addEventListener('click', () => openLightbox(index));
+    galleryContainer.appendChild(imgElement);
+});
+
+
+function openLightbox(index) {
+    currentIndex = index;
+    lightboxImg.src = images[currentIndex].full;
+    captionText.textContent = images[currentIndex].caption;
+    description.textContent = images[currentIndex].description
+    lightbox.classList.add('show');
+    updateNavButtons();
+}
+
+function closeLightbox() {
+    lightbox.classList.remove('show');
+}
