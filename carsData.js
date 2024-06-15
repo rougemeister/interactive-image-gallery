46b788bd2a350e6ac7d7a7ccd080c007af83b7ba
kruzznitsch <rougemeister1@gmail.com>
2024-06-15 17:29:21 +0100

    Moved images data to carsData

---
+++ carsData.js
@@ -0,0 +1,77 @@
+const images = [
+    {
+        full: './images/black-porsche.jpg',
+        thumb: './images/black-porsche.jpg',
+        caption: 'Black Porsche',
+        description: 'A black porsche car riding home!'
+    },
+    {
+        full: './images/blue-chevvy.jpg',
+        thumb: './images/blue-chevvy.jpg',
+        caption: 'A Blue Chevrolet in the desert',
+        description: 'Ride on or Ride Dry!'
+    },
+    {
+        full: './images/gray-coupe.jpg',
+        thumb: './images/gray-coupe.jpg',
+        caption: 'Gray Coupe Sports Cars',
+        description: 'Sleek is the new grey!'
+    },
+    {
+        full: './images/mini-cooper.jpg',
+        thumb: './images/mini-cooper.jpg',
+        caption: 'An Orange Mini Cooper in the City',
+        description: 'Old man in town!'
+    },
+    {
+        full: './images/red-ferrari.jpg',
+        thumb: './images/red-ferrari.jpg',
+        caption: 'Red Ferrari in Showroom',
+        description: 'So long slow riders!'
+    },
+    {
+        full: './images/blue-mustang.jpg',
+        thumb: './images/blue-mustang.jpg',
+        caption: 'Blue Mustang',
+        description: 'I am speed!'
+    },
+    {
+        full: './images/red-american-muscle-car.jpg',
+        thumb: './images/red-american-muscle-car.jpg',
+        caption: 'Red American Muscle Car',
+        description: 'Flex that muscle!'
+    },
+    {
+        full: './images/yellow-lamb.jpg',
+        thumb: './images/yellow-lamb.jpg',
+        caption: 'Yellow Lamborghini',
+        description: `Let's dance!`
+    },
+    {
+        full: './images/white-opentop.jpg',
+        thumb: './images/white-opentop.jpg',
+        caption: 'White Open-top Dogde',
+        description: 'Gentle Jack!'
+    },
+    {
+        full: './images/orange-lamb.jpg',
+        thumb: './images/orange-lamb.jpg',
+        caption: 'Orange Lamborghini',
+        description: 'Ride or die!'
+    },
+    {
+        full: './images/grey-benz.jpg',
+        thumb: './images/grey-benz.jpg',
+        caption: 'Grey Benz Sports Car',
+        description: 'Nothing beats Benz!'
+    },
+    {
+        full: './images/silver-audi.jpg',
+        thumb: './images/silver-audi.jpg',
+        caption: 'Silver Audi',
+        description: 'A car with an impossible speed!'
+    },
+]
+
+
+export default images
